fef0cd0b288e7e17cd355a863a83c4e162c72376
DESKTOP-V63LRML\Admin <s>
2021-03-01 17:25:51 -0600

    testig

--- dummydll/dummydll.h
+++ dummydll/dummydll.h
@@ -1,9 +1,17 @@
 ﻿#pragma once
 
-#ifdef DUMMYDLL_EXPORTS
-#define DUMMYDLL_API __declspec(dllexport)
+
+#if defined (WIN32) && defined (BUILD_SHARED_LIBS)
+#if defined (_MSC_VER)
+#pragma warning(disable: 4251)
+#endif
+#if defined(Dummydll_EXPORTS)
+#define  DUMMYDLL_API __declspec(dllexport)
 #else
-#define DUMMYDLL_API __declspec(dllimport)
+#define  DUMMYDLL_API __declspec(dllimport)
+#endif
+#else
+#define DUMMYDLL_API
 #endif
 
 extern "C"
